02c9dffb4092fba8491c8b88a3e0ae0b88631627
Andrey Felsky <andreyfelsky@edu.univali.br>
2025-04-10 18:36:11 -0300

    Add files via upload
    atualizado

--- main.cpp
+++ main.cpp
@@ -1,46 +1,153 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip> // Para setw e setfill, exibição do tempo de jogo.
+#include "GameElements.cpp"
+#include "GameMap.cpp"
 #include "menu.cpp"
+#include "Combat.cpp"
+
 using namespace std;
 
-int main()
-{   bool exit = false;
-        RenderMenu(MenuItem);
-    while (!exit) {
+int minutes = 0, seconds = 0, score = 0;
 
-        if (_kbhit()) { // Verifica se uma tecla foi pressionada.
-            int key = _getch(); // Captura a tecla.
+void Score(){
+    score = kill * 5;
+    score += quantityOfItemCollected;
+    score += player.key;
+    score -= minutes / 2;
+}
+
+void Game() {
+
+    cout << "\033c" << "Historia do jogo\n\n" << "Pressione 'Enter' para seguir.\n";
+    cin.get();
+    cout << "\033c" << "mais historia bla bla\n\n" << "Pressione 'Enter' para seguir.\n";;
+    cin.get();
+    cout << "\033c";
+    time_t start = time(nullptr);
+    kill = 0;
+
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = false;
+    SetConsoleCursorInfo(out, &cursorInfo);
+    short int CX=0, CY=0;
+    COORD coord;
+    coord.X = CX;
+    coord.Y = CY;
+
+    srand(time(NULL));
+    int rows = 25;
+    int cols = 105;
+
+    GameMap mapa = getRandomMap();
+    VisibleMap vmap;
+    int x = cols / 2, y = rows / 2;
+    char tecla;
+
+    initItems(mapa);
+    vmap.visible[y][x] = true;
+    GameElements elements;
+
+    while (true) {
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        checkItems(mapa, x, y);
+        drawMap(mapa, vmap, x, y);
+
+        drawInfo();
+        if (_kbhit()) {
+            tecla = _getch();
+            switch (tecla) {
+                case 72: case 'w':
+                    if (y > 0 && mapa.tiles[y-1][x] == 0) y--;
+                break;
+                case 80: case 's':
+                    if (y < rows-1 && mapa.tiles[y+1][x] == 0) y++;
+                break;
+                case 75: case 'a':
+                    if (x > 0 && mapa.tiles[y][x-1] == 0) x--;
+                break;
+                case 77: case 'd':
+                    if (x < cols-1 && mapa.tiles[y][x+1] == 0) x++;
+                break;
+                case 27: // Retorna ao menu
+                cout << "\033c" << "Tecla 'ESC' pressionada, jogo encerrado.\n\n";
+                time_t end = time(nullptr);
+                double total_seconds = difftime(end, start);
+                minutes = static_cast<int>(total_seconds) / 60;
+                seconds = static_cast<int>(total_seconds) % 60;
+                Score();
+                cout << "Sua pontuacao foi: " << score << endl << endl;
+                cout << "Vida: " << player.life << " | Itens Coletados: " << quantityOfItemCollected << "/" << MAX_ITEMS << " | Chaves Coletadas: " << player.key << endl;
+                cout << "\nTempo de Jogo: " << minutes << ":" << setw(2) << setfill('0') << seconds << " minutos\n\n";
+                cout << "Pressione 'Enter' para voltar ao Menu." << endl;
+                cin.get();
+                cout << "\033c";
+                return;
+                }
+            }
+        }
+    }
+
+
+
+
+
+        // Player player = {3, 1, 1, {itens[0], itens[1], itens[2], itens[3]}};
+    // int coutEnemie = 1;
+    // Npc enemies[coutEnemie];
+    // generateEnemies(enemies, coutEnemie);
+    // Combatant infoCombat[maxCombatants];
+    // generateInitiatives(infoCombat, enemies, coutEnemie, player);
+    // int totalCombatants = coutEnemie + 1;
+
+    // combatMenu(infoCombat, totalCombatants, player);
+    // return 0;
+
+int main() {
+    bool exit = false;
+    RenderMenu(MenuItem);
+
+    while (!exit) {
+        if (_kbhit()) {
+            int key = _getch();
             switch (key) {
-                case 72: case 'w': case 'W': // Seta para cima ou W.
+                case 72: case 'w': case 'W':
                     if (MenuItem == 0){
                         MenuItem = 3;
-                        }else{
+                    } else {
                         MenuItem -= 1;
-                        }
+                    }
                     break;
-                case 80: case 's': case 'S': // Seta para baixo ou S.
+                case 80: case 's': case 'S':
                     if (MenuItem == N_OPCOES-1){
                         MenuItem = 0;
-                        }else {
+                    } else {
                         MenuItem +=1;
-                        }
+                    }
                     break;
-                case 13: case 32: // Detecta se foi pressionado a tecla Enter ou EspaÃ§o, e exibe na tela o menu do item selecionado.
-                    if (Opcoes[MenuItem] == "Jogar"){
-                    Game();
-                    }if (Opcoes[MenuItem] == "Como Jogar"){
-                    instructiongame();
-                    }if (Opcoes[MenuItem] == "Itens"){
-                    Itens();
-                    }if (Opcoes[MenuItem] == "Sair") {
-                    cout << "Saindo...\n";
-                    exit = true;
-                    }break;
-                }RenderMenu(MenuItem);
+                case 13: case 32:
+                    if (Opcoes[MenuItem] == "  Jogar   ") {
+                        system("cls");
+                        Game();
+                        system("cls");
+                        RenderMenu(MenuItem);
+                    } else if (Opcoes[MenuItem] == "Como Jogar") {
+                        instructiongame();
+                    } else if (Opcoes[MenuItem] == "  Itens   ") {
+                        Itens();
+                    } else if (Opcoes[MenuItem] == "  Sair    ") {
+                        cout << "Saindo...";
+                        exit = true;
+                    }
+                    break;
+            }
+            RenderMenu(MenuItem);
         }
-    }return 0;
+    }
+    return 0;
 }
--- menu.cpp
+++ menu.cpp
@@ -1,72 +1,9 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
-#include <cstdlib>
-#include <ctime>
-#include "GameElements.cpp"
-#include "GameMap.cpp"
-#define GAMEELEMENTS_H
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
-
-void Game(){
-
-    cout << "\033c" << "Historia do jogo\n" << "Pressione Enter para seguir.\n";
-    cin.get();
-    cout << "\033c" << "mais historia bla bla\n" << "Pressione Enter para seguir.\n";;
-    cin.get();
-    cout << "\033c";
-
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO cursorInfo;
-    GetConsoleCursorInfo(out, &cursorInfo);
-    cursorInfo.bVisible = false;
-    SetConsoleCursorInfo(out, &cursorInfo);
-    short int CX=0, CY=0;
-    COORD coord;
-    coord.X = CX;
-    coord.Y = CY;
-
-    srand(time(NULL));
-    int rows = 25;
-    int cols = 105;
-
-    GameMap mapa = getRandomMap();
-    VisibleMap vmap;
-    int x = cols / 2, y = rows / 2;
-    char tecla;
-
-    initItems(mapa);
-    vmap.visible[y][x] = true;
-    GameElements elements;
-
-    while (true) {
-
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-        checkItems(mapa, x, y);
-        drawMap(mapa, vmap, x, y);
-        // bossRoom(mapFinal,  x, y); //falta implementar a logica do jogo na sala do boss
-
-        drawInfo();
-        if (_kbhit()) {
-            tecla = _getch();
-            switch (tecla) {
-                case 72: case 'w':
-                    if (y > 0 && mapa.tiles[y-1][x] == 0) y--;
-                break;
-                case 80: case 's':
-                    if (y < rows-1 && mapa.tiles[y+1][x] == 0) y++;
-                break;
-                case 75: case 'a':
-                    if (x > 0 && mapa.tiles[y][x-1] == 0) x--;
-                break;
-                case 77: case 'd':
-                    if (x < cols-1 && mapa.tiles[y][x+1] == 0) x++;
-                break;
-            }
-        }
-
-    }
-}
 
 void ClearConsole() {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -89,6 +26,7 @@ void instructiongame(){
     cout << "\033c";
     cout << "  Jogar\n> Como Jogar\n  Itens\n  Sair\n";// Sai do jogo e volta pro menu.
 }
+
  void Itens(){
     cout << "\033c";
     cout << ">> ITENS <<\n\n";
